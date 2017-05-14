f43df360ab06a1ec13ceb4229b3e0c951a14d128
Will Cook <hello@doubleucook.com>
2017-05-14 14:55:06 +0100

    cleaned up server file

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app     = express();
 const port    = process.env.PORT || 8000;
-const routes  = require("./config/routes");
+const path = require('path');
 
 app.use(express.static(`${__dirname}/public`));
 
-app.get("/", routes);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './index.html'));
+});
 
 app.listen(port, () =>{
-  console.log("Server has started on port " + port);
+  console.log(`Server has started on port ${port}`);
 });
